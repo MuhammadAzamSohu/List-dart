118f41bf262eef5be28e0776f5337df2d6b4432b
MuhammadAzamSohu <muhammadazamsohu36@gmail.com>
2023-06-23 01:03:17 +0500

    remove names

---
+++ removedays.dart
@@ -0,0 +1,26 @@
+void main() {
+  List<String> days = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday"
+  ];
+
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+  days.removeLast();
+  print(days);
+}
